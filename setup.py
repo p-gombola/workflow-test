c3b97ce1a988a2202b137f8524ee7df4f3a9d51c
Patrick Gombola <pgombola@cloudbees.com>
2024-03-18 19:50:57 -0400

    sample python starter

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='StarterStuff',
+    version='0.1dev',
+    packages=['starter',],
+    license='No license',
+    long_description='This is just a sample project to see if it builds',
+)
