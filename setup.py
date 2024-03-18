ace49c1fc594d9e528c6e763e515b9d62dfb35d7
Patrick Gombola <pgombola@cloudbees.com>
2024-03-18 19:57:16 -0400

    set packages to pipenv

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='StarterStuff',
     version='0.1dev',
-    packages=['starter',],
+    packages=['pipenv',],
     license='No license',
     long_description='This is just a sample project to see if it builds',
 )
